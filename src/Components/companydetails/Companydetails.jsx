ba89c3aace79c6aa0e2bc62daa48d07baf4c9953
Onayifeke Edison Oghenerunor <tkeddy5@gmail.com>
2023-05-19 04:52:12 +0100

    Updating app

--- src/Components/companydetails/Companydetails.jsx
+++ src/Components/companydetails/Companydetails.jsx
@@ -1,32 +1,7 @@
-import { Component } from "react";
-import "./companydetails.scss";
-import Companydetailsinfo from "./Companydetailsinfo.jsx";
-import { Slide } from "react-awesome-reveal";
-import companyData from "./companydetails.json";
+import React from "react";
 
-const data = companyData;
-
-class Companydetails extends Component {
-  render() {
-    return (
-      <div className="companydetails">
-        <h3>affordable housing at your finger tips</h3>
-        <div className="about-company">
-          {data.map((dat, i) => {
-            return (
-              <Slide direction="bottom">
-                <Companydetailsinfo
-                  pics={dat.pics}
-                  subHeading={dat.subHeading}
-                  text={dat.text}
-                />
-              </Slide>
-            );
-          })}
-        </div>
-      </div>
-    );
-  }
+function Companydetails() {
+  return <div>Companydetails</div>;
 }
 
 export default Companydetails;
